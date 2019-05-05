0bb502079d5169867541849fbf5c8ccb26cee64a
Sasete <SaseteS@icloud.com>
2019-05-05 10:33:35 +0300

    Type mistake
    Fixed Type mistake

--- Codes/ClientListener.py
+++ Codes/ClientListener.py
@@ -40,10 +40,10 @@ sock.listen(1)
 
 conn, addr = s.accept()
 
-print 'Connection address:' addr
+print ('Connection address:' addr)
 while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
-    print "received data:", data
+    print ("received data:", data)
     conn.send(data)
 conn.close
